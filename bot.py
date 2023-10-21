34a76f22c8ebc26b11aecaca1e3e6e2e357d0577
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-22 00:41:00 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,70 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import requests
+from bs4 import BeautifulSoup
+
+# Replace with your actual API ID, API hash, and bot token
+api_id = "19099900"
+api_hash = "2b445de78e5baf012a0793e60bd4fbf5"
+bot_token = "6062250856:AAEI_suUfM-_MKhJT5cODnWgYJbkH3To9o4"
+
+# Create the Pyrogram client
+app = Client(
+    "pinterest_bot",
+    api_id=api_id,
+    api_hash=api_hash,
+    bot_token=bot_token
+)
+
+@app.on_message(filters.command("download"))
+async def download_pinterest_images(client: Client, message: Message):
+    try:
+        # Get the user's query from the message text after the /download command
+        query = " ".join(message.text.split()[1:])
+
+        if not query:
+            await message.reply("Please provide a search query after /download.")
+            return
+
+        # Search Pinterest for the user's query
+        search_url = f"https://www.pinterest.com/search/pins/?q={query}"
+        response = requests.get(search_url)
+
+        if response.status_code == 200:
+            # Parse the HTML content of the search results
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Find up to 5 image elements in the search results
+            img_elements = soup.find_all("img")
+
+            if img_elements:
+                # Send up to 5 images
+                for i, img_element in enumerate(img_elements[:5]):
+                    image_url = img_element.get("src")
+
+                    # Download the image
+                    img_response = requests.get(image_url)
+
+                    if img_response.status_code == 200:
+                        with open(f"downloaded_image_{i + 1}.jpg", "wb") as file:
+                            file.write(img_response.content)
+
+                        # Send the downloaded image
+                        await client.send_photo(
+                            chat_id=message.chat.id,
+                            photo=f"downloaded_image_{i + 1}.jpg",
+                            caption=f"Image {i + 1} for query: {query}"
+                        )
+
+                await message.reply("Images downloaded and sent successfully.")
+            else:
+                await message.reply(f"No images found for query: {query}.")
+        else:
+            await message.reply("Failed to access Pinterest search results.")
+    except Exception as e:
+        await message.reply(f"An error occurred: {str(e)}")
+
+if __name__ == "__main__":
+    with app:
+        app.run()
+      
